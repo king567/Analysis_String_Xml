489346e15457910be09d1b4016286376a1f338c4
WIJTB <104023005@live.asia.edu.tw>
2018-09-03 17:02:53 +0800

    beta

--- Analysis_String_Xml/Form1.Designer.cs
+++ Analysis_String_Xml/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Get_Content_Bt = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Get_Content_Bt
+            // 
+            this.Get_Content_Bt.Location = new System.Drawing.Point(240, 12);
+            this.Get_Content_Bt.Name = "Get_Content_Bt";
+            this.Get_Content_Bt.Size = new System.Drawing.Size(187, 61);
+            this.Get_Content_Bt.TabIndex = 0;
+            this.Get_Content_Bt.Text = "取出內容";
+            this.Get_Content_Bt.UseVisualStyleBackColor = true;
+            this.Get_Content_Bt.Click += new System.EventHandler(this.Get_Content_Bt_ClickAsync);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 89);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(469, 327);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(753, 428);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Get_Content_Bt);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Get_Content_Bt;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Analysis_String_Xml/Form1.cs
+++ Analysis_String_Xml/Form1.cs
@@ -5,8 +5,11 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using YandexTranslateCSharpSdk;
 
 namespace Analysis_String_Xml
 {
@@ -15,6 +18,40 @@ namespace Analysis_String_Xml
         public Form1()
         {
             InitializeComponent();
+        }
+        public void Insert_Text(string content)
+        {
+            int count = textBox1.Text.Length;
+            string get_box_text = textBox1.Text;
+            textBox1.Text = get_box_text.Insert(count, content + "\r\n");
+            //////自動卷軸到最底下
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.SelectionStart = textBox1.Text.Length;
+            textBox1.ScrollToCaret();
+        }
+        private async void Get_Content_Bt_ClickAsync(object sender, EventArgs e)
+        {
+            YandexTranslateSdk wrapper = new YandexTranslateSdk();
+            wrapper.ApiKey = "trnsl.1.1.20180826T084827Z.54e49a82ea8c2f4d.3459b97eef4275c517282942408b6e377c9bee8b";
+            string xml_path = @"D:\Users\WIJTB\Desktop\test\strings.xml";
+            XmlDocument XmlDoc = new XmlDocument();
+            XmlDoc.Load(xml_path);
+            XmlNodeList NodeLists = XmlDoc.SelectNodes("resources/string");
+            foreach (XmlNode OneNode in NodeLists)
+            {
+                String StrAttrValue = OneNode.InnerText;
+                Regex regex = new Regex(@"(\$|%)[0-9A-Za-z]");
+                Match m = regex.Match(StrAttrValue);
+                if (m.Success)
+                {
+
+                }
+                else
+                {
+                    string translatedText = await wrapper.TranslateText(StrAttrValue, "zh");
+                    Insert_Text(translatedText);
+                }
+            }
         }
     }
 }
